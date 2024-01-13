b855f98bc4c98fc054e5c04fcaab505d09a5bb06
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-01-13 15:34:50 +0700

    revert to nuxtjs

--- .vitepress/config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "Hello, Chan Dinh!",
-  description: "Welcome to my personal blog where I share what I learn and how I feel",
-})
