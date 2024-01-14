60b84c9dc6d231eb0d12d02651c95c643a552329
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-01-14 14:31:58 +0700

    add font and re organize home components

---
+++ components/AppHomeMain.vue
@@ -0,0 +1,29 @@
+<template>
+<main>
+      <div class="py-4">
+        <h2 class="text-xl font-bold text-red-500 underline"><a href="/2019/09/23/setting-up-a-server-for-serverless.html" class="text-secondary">Setting Up a Server for Serverless</a></h2>
+        <h3 class="font-light text-gray-700">Even for serverless, you sometimes need a server!</h3>
+        <h3 class="text-xs font-light italic py-1">23 September 2019 by Martin Dawson</h3>
+      </div>
+      <div class="py-4">
+        <h2 class="text-xl font-bold text-red-500 underline"><a href="/2019/09/20/getting-started-with-jekyll.html" class="text-secondary">Getting Started with Jekyll</a></h2>
+        <h3 class="font-light text-gray-700">Jekyll got me in to Static Site Generators, and remains a sensible choice for many types of website.</h3>
+        <h3 class="text-xs font-light italic py-1">20 September 2019 by Martin Dawson</h3>
+      </div>
+      <div class="py-4">
+        <h2 class="text-xl font-bold text-red-500 underline"><a href="/2019/09/14/autumn.html" class="text-secondary">Autumn</a></h2>
+        <h3 class="font-light text-gray-700">One of my favourite episodes of the Reply All podcast.</h3>
+        <h3 class="text-xs font-light italic py-1">14 September 2019 by Martin Dawson</h3>
+      </div>
+      <div class="py-4">
+        <h2 class="text-xl font-bold text-red-500 underline"><a href="/2019/09/13/a-loosener.html" class="text-secondary">A Loosener...</a></h2>
+        <h3 class="font-light text-gray-700">A short post to kick things off.</h3>
+        <h3 class="text-xs font-light italic py-1">13 September 2019 by Martin Dawson</h3>
+      </div>
+      <div class="py-4">
+        <h2 class="text-xl font-bold text-red-500 underline"><a href="/2019/09/09/the-most-important-thing-every-website-needs.html" class="text-secondary">The Most Important Thing Every Website Needs</a></h2>
+        <h3 class="font-light text-gray-700">Without this, you have nothing!</h3>
+        <h3 class="text-xs font-light italic py-1">9 September 2019 by Martin Dawson</h3>
+      </div>
+    </main>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900;1000&display=swap' }
+      ]
+    }
+  },
+  css: ['~/assets/styles.css']
 })
