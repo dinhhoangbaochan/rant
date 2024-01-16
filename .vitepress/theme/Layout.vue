25f35cee4ae11712b79dc3fac4aeec4152c15522
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-01-16 22:13:08 +0700

    update basic infor

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -15,31 +15,18 @@ const { page, frontmatter } = useData()
           <span
             v-if="!frontmatter.index"
             class="hidden md:inline dark:text-white"
-            >The Vue Point</span
+            >Hello, Chan Dinh</span
           >
         </a>
         <div class="text-sm text-gray-500 dark:text-white leading-5">
           <a
             class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="https://github.com/vuejs/blog"
+            href="https://github.com/dinhhoangbaochan/hello-chandinh"
             target="_blank"
             rel="noopener"
             ><span class="hidden sm:inline">GitHub </span>Source</a
           >
           <span class="mr-2 ml-2">·</span>
-          <a
-            class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="/feed.rss"
-            >RSS<span class="hidden sm:inline"> Feed</span></a
-          >
-          <span class="mr-2 ml-2">·</span>
-          <a
-            class="hover:text-gray-700 dark:hover:text-gray-200"
-            href="https://vuejs.org"
-            target="_blank"
-            rel="noopener"
-            >Vuejs.org →</a
-          >
         </div>
       </nav>
     </div>
