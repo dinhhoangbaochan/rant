43f7ded0ef804e254907f5f3dedd59f54a57ea0b
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-01-16 22:10:30 +0700

    config tailwind

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,21 +1,52 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
+// import Home from './Home.vue'
+// import Article from './Article.vue'
+// import NotFound from './NotFound.vue'
 
-// https://vitepress.dev/reference/runtime-api#usedata
-const { site, frontmatter } = useData()
+const { page, frontmatter } = useData()
 </script>
 
 <template>
-  <div v-if="frontmatter.home">
-    <h1>{{ site.title }}</h1>
-    <p>{{ site.description }}</p>
-    <ul>
-      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
-      <li><a href="/api-examples.html">API Examples</a></li>
-    </ul>
-  </div>
-  <div v-else>
-    <a href="/">Home</a>
-    <Content />
+  <div class="antialiased dark:bg-slate-900">
+    <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <nav class="flex justify-between items-center py-10 font-bold">
+        <a class="text-xl" href="/" aria-label="The Vue Point">
+          <span
+            v-if="!frontmatter.index"
+            class="hidden md:inline dark:text-white"
+            >The Vue Point</span
+          >
+        </a>
+        <div class="text-sm text-gray-500 dark:text-white leading-5">
+          <a
+            class="hover:text-gray-700 dark:hover:text-gray-200"
+            href="https://github.com/vuejs/blog"
+            target="_blank"
+            rel="noopener"
+            ><span class="hidden sm:inline">GitHub </span>Source</a
+          >
+          <span class="mr-2 ml-2">·</span>
+          <a
+            class="hover:text-gray-700 dark:hover:text-gray-200"
+            href="/feed.rss"
+            >RSS<span class="hidden sm:inline"> Feed</span></a
+          >
+          <span class="mr-2 ml-2">·</span>
+          <a
+            class="hover:text-gray-700 dark:hover:text-gray-200"
+            href="https://vuejs.org"
+            target="_blank"
+            rel="noopener"
+            >Vuejs.org →</a
+          >
+        </div>
+      </nav>
+    </div>
+    <main class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <Home v-if="frontmatter.index" />
+      <NotFound v-else-if="page.isNotFound" />
+      <Article v-else />
+    </main>
   </div>
 </template>
