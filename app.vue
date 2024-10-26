8e58d62232ad97101b7f14df46b0a8884b66b6a7
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-10-26 18:24:04 +0700

    feature: add basic header

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <h1>Hello, Chan Dinh</h1>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+    <div class="container">
+      <Header />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
