05e6770b550e2cb128b2833be306169f0c088f3d
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-10-26 18:35:45 +0700

    feature: add sample post list

---
+++ components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list">
+    <div class="post-item" v-for="post in postList">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const postList = [
+  {
+    id: 0,
+    title: "First post",
+    description: "This is the description for my first post!"
+  },
+
+  {
+    id: 1,
+    title: "Second post",
+    description: "This is the description for my second post!"
+  },
+]
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
     <div class="container">
       <Header />
       <Hero />
+      <PostList />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
